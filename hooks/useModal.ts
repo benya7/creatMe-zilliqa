f8d8dc0f09bc3f35f071f359f4cb4cc824f47dee
en0c-026 <bitup.games@gmail.com>
2022-06-28 17:19:33 +0200

    add useModal hook

---
+++ hooks/useModal.ts
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+export default function useModal() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const showModal = () => setIsVisible(true);
+  const hideModal = () => setIsVisible(false);
+
+  return { isVisible, showModal, hideModal };
+}
