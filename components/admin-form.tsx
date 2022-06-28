e0d6e03c307dad00595c15496392b34d5541bc94
en0c-026 <bitup.games@gmail.com>
2022-06-28 19:39:51 +0200

    add Mint and BatchMint functions in Admin page

--- components/admin-form.tsx
+++ components/admin-form.tsx
@@ -1,4 +1,4 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { Network, Transition, TransitionName } from "../lib/types";
 
 interface FormProps {
@@ -61,15 +61,18 @@ export default function AdminForm({
               required
               className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
               onChange={({ target: { value } }) => setTransition({ name: (value as TransitionName), params: {} })}
-              value={transition?.name}
+              value={transition?.name ? transition.name : "Select a Transition"}
             >
-              <option selected value="Pause">Pause</option>
+              <option>Select a Transition</option>
+              <option value="Pause">Pause</option>
               <option value="Unpause">Unpause</option>
               <option value="SetRoyaltyRecipient">SetRoyaltyRecipient</option>
               <option value="SetRoyaltyFeeBPS">SetRoyaltyFeeBPS</option>
               <option value="SetBaseURI">SetBaseURI</option>
               <option value="AddMinter">AddMinter</option>
               <option value="RemoveMinter">RemoveMinter</option>
+              <option value="Mint">Mint</option>
+              <option value="BatchMint">BatchMint</option>
               <option value="SetContractOwnershipRecipient">SetContractOwnershipRecipient</option>
               <option value="AcceptContractOwnership">AcceptContractOwnership</option>
 
@@ -82,8 +85,6 @@ export default function AdminForm({
             )
           }
 
-
-
           <button
             className="mt-2 px-4 py-2 border bg-emerald-300 rounded-xl hover:bg-emerald-500 disabled:bg-gray-300"
             type="submit"
@@ -91,6 +92,40 @@ export default function AdminForm({
           >
             Submit
           </button>
+          {
+            transition &&
+            transition.params &&
+            transition.params.to_token_uri_pair_list &&
+            transition.params.to_token_uri_pair_list[1].length > 0 && (
+              <div className="grid grid-cols-1 mt-4 gap-2 ">
+                <span className="text-gray-700">Destination List</span>
+                <ul className="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg">
+                  {
+                    transition.params.to_token_uri_pair_list[1].map((
+                      item: string[],
+                      i: number
+                    ) => (
+                      item && (
+
+                        <li key={i} className="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">
+                          <div className="flex gap-2 justify-between">
+                            <span>To:</span>
+                            <span className="text-xs">{item[0]}</span>
+                          </div>
+                          <div className="flex gap-2 justify-between">
+                            <span>URI:</span>
+                            <span className="text-xs">{item[1]}</span>
+                          </div>
+                        </li>
+                      )
+
+                    ))
+                  }
+
+                </ul>
+              </div>
+            )
+          }
         </div>
       </div>
     </form>
@@ -252,10 +287,143 @@ const Params = ({
             placeholder=""
           />
         </label>
+
       </div>
+    )
+  } else if (transition.name === "Mint") {
+    return (
+      <div>
+        <span className="text-gray-700">Params:</span>
+        <label className="block" htmlFor="to">
+          <span className="text-gray-700 text-sm">To</span>
+          <input
+            type="text"
+            id="to"
+            name="to"
+            required
+            value={transition.params?.to ? transition.params.to[1] : ""}
+            onChange={({ target: { value } }) => setTransition({
+              ...transition,
+              params: {
+                ...transition.params,
+                to: ["ByStr20", value]
+              }
+            })
+            }
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            placeholder=""
+          />
+        </label>
+        <label className="block" htmlFor="token_uri">
+          <span className="text-gray-700 text-sm">Token URI</span>
+          <input
+            type="text"
+            id="token_uri"
+            name="token_uri"
+            required
+            value={transition.params?.token_uri ? transition.params.token_uri[1] : ""}
+            onChange={({ target: { value } }) => setTransition({
+              ...transition,
+              params: {
+                ...transition.params,
+                token_uri: ["String", value]
+              }
+            })
+            }
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            placeholder=""
+          />
+        </label>
+      </div>
+    )
+  } else if (transition.name === "BatchMint") {
+    return (
+      <BatchMint
+        transition={transition}
+        setTransition={setTransition}
+      />
     )
   }
   else {
     return null
   }
+}
+
+
+
+const BatchMint = ({
+  transition,
+  setTransition
+}: {
+  transition: Partial<Transition>;
+  setTransition: (value: Partial<Transition>) => void;
+}) => {
+  const [to, setTo] = useState<string>();
+  const [tokenURI, setTokenURI] = useState<string>();
+
+  const handleOnClickAddDest = () => {
+    if (!to || to === "" || !tokenURI || tokenURI === "") return;
+    setTransition({
+      ...transition,
+      params: {
+        to_token_uri_pair_list: [
+          "List (Pair (ByStr20) (String))",
+          [
+            ...transition.params?.to_token_uri_pair_list?.[1] ? transition.params.to_token_uri_pair_list[1] : [null, null],
+            [to, tokenURI]
+          ]
+        ]
+      }
+    })
+  }
+  const clearListDest = () => {
+    setTransition({
+      ...transition,
+      params: undefined
+    })
+  }
+
+
+  return (
+    <div className="grid grid-cols-1 gap-4" >
+
+      <label className="block" htmlFor="to">
+        <span className="text-gray-700">to</span>
+        <input
+          type="text"
+          name="to"
+          id="to"
+          required
+          value={to ? to : ""}
+          onChange={(e) => setTo(e.target.value)}
+          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+        />
+      </label>
+      <label className="block" htmlFor="tokenURI">
+        <span className="text-gray-700">Token URI</span>
+        <input
+          type="text"
+          min={0}
+          name="tokenURI"
+          id="tokenURI"
+          required
+          value={tokenURI ? tokenURI : ""}
+          onChange={(e) => setTokenURI(e.target.value)}
+          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+        />
+      </label>
+      <div className="flex flex-1 gap-2 justify-around">
+        <button
+          className="px-2 py-1 rounded-lg text-sm bg-cyan-600 hover:bg-cyan-700 text-slate-50 disabled:bg-slate-500" onClick={handleOnClickAddDest}
+          type='button'
+          disabled={!to || to === "" || !tokenURI || tokenURI === ""}>
+          Add Destination
+        </button>
+        <button className="px-2 py-1 rounded-lg text-sm bg-orange-600 hover:bg-orange-700 text-slate-50 disabled:bg-slate-500" onClick={clearListDest} type='button'>
+          Clear List
+        </button>
+      </div>
+    </div>
+  )
+
 }
