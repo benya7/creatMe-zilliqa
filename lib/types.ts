68116c64b08f28e1dbc061211adac71c605578a4
en0c-026 <bitup.games@gmail.com>
2022-06-28 17:19:05 +0200

    update file types

---
+++ lib/types.ts
@@ -0,0 +1,46 @@
+export type TransitionName =
+  "Pause" |
+  "Unpause" |
+  "SetRoyaltyRecipient" |
+  "SetRoyaltyFeeBPS" |
+  "SetBaseURI" |
+  "Mint" |
+  "BatchMint" |
+  "Burn" |
+  "BatchBurn" |
+  "AddMinter" |
+  "RemoveMinter" |
+  "SetSpender" |
+  "AddOperator" |
+  "RemoveOperator" |
+  "TransferFrom" |
+  "BatchTransferFrom" |
+  "SetContractOwnershipRecipient" |
+  "AcceptContractOwnership"
+
+export type Params = {
+  [key: string]: [string, any];
+}
+
+export interface BaseRequest {
+  network: "testnet" | "mainnet";
+}
+
+export type Transition = {
+  name: TransitionName;
+  params: Params;
+}
+
+export interface NewRequest extends BaseRequest {
+  name: string;
+  symbol: string;
+  initialUri?: string;
+  
+}
+
+export interface AdminRequest extends BaseRequest {
+  contractAddress: string;
+  transition: Transition;
+}
+
+export type Network = "testnet" | "mainnet"
